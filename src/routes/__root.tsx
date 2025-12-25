d247ebad65157b3d9017163c6a28bff94b3365bc
freshfriedfish <shafayatm2@gmail.com>
2025-12-25 16:15:45 -0500

    added shadcn compoennts

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,7 +2,7 @@ import { Outlet, createRootRoute } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="px-4">
+    <div className="">
       <Outlet />
     </div>
   ),
--- src/routes/metal-pipe/index.tsx
+++ src/routes/metal-pipe/index.tsx
@@ -1,7 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 function MetalPipePage() {
-  return null
+  return (
+    <div className="min-h-screen bg-radial-blue  h-full mx-auto flex justify-center items-center">
+      <h1 className="bg-linear-to-r from-gray-300 to-gray-600 bg-clip-text text-5xl font-bold text-transparent">
+        Metal Pipe
+      </h1>
+    </div>
+  )
 }
 
 export const Route = createFileRoute('/metal-pipe/')({
